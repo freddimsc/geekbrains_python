f8011a7a722e2dfdfbc5783d07709fce028dfdea
Александр Большаков <fredq@yandex.ru>
2020-12-18 23:10:24 +0300

    [upd] Николай привет, хотел добавить на вывод текс с файла чтоб наглядней было, но он у меня не вмещался, так получалось, либо подсчеты со строками, либо текст, я так понял в самом pycharm загвоздка, хотя хз, принтанул ток кол строк и кол слов в строке, как по задаче
    Все получилось, надо было seek(0) сделать , в начало файла)))

--- lesson5/hw_5_2.py
+++ lesson5/hw_5_2.py
@@ -2,7 +2,9 @@
 # сохранить в нем несколько строк, выполнить подсчет количества строк, количества слов в каждой строке.
 
 with open('text_test.txt',"r") as text_t:
-
+    text = text_t.read()
+    print(f"текст с нашего файла : \n {text}")
+    text_t.seek(0)
     content = text_t.readlines()
     print(f'Количество строк в файле - {len(content)} ')
     for j,i in enumerate(content):
